2638cdaabe0c3434a023c383bf8446172f5f77d8
Lili <jamila.begum@vialto.com>
2023-02-16 14:34:18 +0000

    Updating ReadMe whilst taking a break from logic.js file

--- README.md
+++ README.md
@@ -1,1 +1,46 @@
-# wk6-webAPI-quiz
+# WebAPI quiz
+Advanced Javascript Challenge - creating a quiz
+
+
+## Description
+Using skills learnt, I am aiming to build a timed coding quiz with multiple-choice questions. This app will run in the browser, and will feature dynamically updated HTML and CSS powered by JavaScript code that I write.
+
+
+## Table of Contents
+
+* [Deployed Page](#deployed-page)
+* [Process](#process)
+* [Additional Notes](#additional-notes)
+* [Credits](#credits)
+* [License](#license)
+
+# Deployed Page
+
+* [Deployed Page Link](https://lilibear1.github.io/wk6-webAPI-quiz/)
+
+    !["Coding quiz start page preview"](/assets/images/Start.png)
+
+* [Git Page Link](https://github.com/LiliBear1/wk6-webAPI-quiz) 
+
+
+
+## Process
+
+This website is to display advanced JavaScript Skills. When the user clicks the "Start" button, they will go through a series of timed questions in which they will lose 10 seconds everytime the get a question wrong, at the end of the game the user will see their score and save this with their initials alongside a leaderboard.
+
+I started off strong with my Questions file, representing each question as an object in an array. I then created a Scores and Logic file which would tie everything together and display the scores.
+
+!["Demo of quiz in action"](/assets/images/.png)
+
+## Additional notes
+
+I used a combination of objects, arrays, functions, for loops, while and if statements as well as learnt how to call and return functions as well as various operators.
+
+The biggest challenge I faced was the Logic page as this was the most detailed one, I got hit by data fatigue and would have got bogged down with uncaught syntax errors due to staring at a page of code too long, there is a large amount of information on the internet which can become overwhelming, therefore I did my commits frequently, stepped away and worked on other sections and then came back.
+
+## Credits 
+2022 Trilogy Education Services, LLC
+
+## License
+
+An [MIT license](https://choosealicense.com/licenses/mit/) was used.
--- assets/js/logic.js
+++ assets/js/logic.js
@@ -21,7 +21,7 @@ var ul = document.createElement("ul");
 
 // Start quiz timer on button click.
 // When clicked a timer starts and the first question appears
- 
+
 startQuiz.addEventListener("click", function () {
   if (interval === 0) {
     interval = setInterval(function () {
@@ -37,7 +37,6 @@ startQuiz.addEventListener("click", function () {
   }
   render(questionIndex);
 });
-
 
 // Consider audio sound for correct being added here, MDN doc on Audio APIs
 // Render questions and choices:
@@ -81,105 +80,105 @@ function compare(event) {
       score++;
       createDiv.textContent =
         "Correct! The answer is:  " + questions[questionIndex].answer;
-
-        // If the answer clicked was incorrect
-        // Consider audio sound for incorrect being added here, MDN doc on Ausio APIs
-    } else {
-      createDiv.setAttribute("id", "createDivIncorrect");
-      setTimeout(() => {
-        createDiv.style.display = "none";
-      }, 1000); // milliseconds
-
-      // Deduct 10 seconds for wrong answers
-      secondsRemain = secondsRemain - penalty;
-      createDiv.textContent =
-        "Wrong! The correct answer is:  " + questions[questionIndex].answer;
-    }
-  }
-  // Increment questions and check for quiz end condition, then end when met
-  questionIndex++;
-
-  if (questionIndex >= questions.length) {
-    createDiv.setAttribute("id", "createDiv");
+    }  else {
+  
+  // If the answer clicked was incorrect
+  // Consider audio sound for incorrect being added here, MDN doc on Ausio APIs
+    createDiv.setAttribute("id", "createDivIncorrect");
     setTimeout(() => {
       createDiv.style.display = "none";
-    }, 1000);
+    }, 1000); // milliseconds
 
-    //   When the game ends: It should display their score, Give the user the ability to Save their initials and Save their score 
-    function allDone() {
-        questionsDiv.innerHTML = "";
-        clearInterval(interval);
-      
-        var createH1 = document.createElement("h1");
-        createH1.setAttribute("id", "createH1");
-        createH1.textContent = "All Done!";
-      
-        questionsDiv.appendChild(createH1);
-      
-        var createP = document.createElement("p");
-        createP.setAttribute("id", "createP");
-      
-        questionsDiv.appendChild(createP);
-      
-        // Calculate final score and display
-        var finalScore = secondsRemain;
-      
-        var createSpan = document.createElement("span");
-        createSpan.setAttribute("id", "createSpan");
-        createSpan.textContent = finalScore;
-      
-        createP.appendChild(createSpan);
-      
-        // User to enter initials in field
-        var createLabel = document.createElement("label");
-        createLabel.setAttribute("id", "createLabel");
-        createLabel.textContent = "Enter your initials: ";
-      
-        questionsDiv.appendChild(createLabel);
-      
-        var createInput = document.createElement("input");
-        createInput.setAttribute("type", "text");
-        createInput.setAttribute("id", "initials");
-        createInput.textContent = "";
-      
-        questionsDiv.appendChild(createInput);
-      
-        // Button to submit initials and score
-        var createSubmit = document.createElement("button");
-        createSubmit.setAttribute("type", "submit");
-        createSubmit.setAttribute("id", "submit");
-        createSubmit.textContent = "Submit";
-      
-        questionsDiv.appendChild(createSubmit);
-      
-        // Event listener for submitting initials and score
-        createSubmit.addEventListener("click", function () {
-          var initials = createInput.value;
-      
-          // Ensure initials are not blank
-          if (initials === "") {
-            alert("Please enter your initials.");
-          } else {
-            // Get existing high scores from local storage or create empty array
-            var highScores = JSON.parse(localStorage.getItem("highScores")) || [];
-      
-            // Add new high score object to the high scores array
-            var newScore = {
-              initials: initials,
-              score: finalScore,
-            };
-      
-            highScores.push(newScore);
-      
-            // Sort high scores in descending order
-            highScores.sort(function (a, b) {
-              return b.score - a.score;
-            });
-      
-            // Save updated high scores to local storage
-            localStorage.setItem("highScores", JSON.stringify(highScores));
-      
-            // Redirect to high scores page
-            window.location.href = "highscores.html";
-          }
-    });
+    // Deduct 10 seconds for wrong answers
+    secondsRemain = secondsRemain - penalty;
+    createDiv.textContent =
+      "Wrong! The correct answer is:  " + questions[questionIndex].answer;
+  }
+}
+// Increment questions and check for quiz end condition, then end when met
+questionIndex++;
+
+if (questionIndex >= questions.length) {
+  createDiv.setAttribute("id", "createDiv");
+  setTimeout(() => {
+    createDiv.style.display = "none";
+  }, 1000);
+
+  // When the game ends: It should display their score, Give the user the ability to Save their initials and Save their score
+  function allDone() {
+    questionsDiv.innerHTML = "";
+    clearInterval(interval);
+
+    var createH1 = document.createElement("h1");
+    createH1.setAttribute("id", "createH1");
+    createH1.textContent = "All Done!";
+
+    questionsDiv.appendChild(createH1);
+
+    var createP = document.createElement("p");
+    createP.setAttribute("id", "createP");
+
+    questionsDiv.appendChild(createP);
+
+    // Calculate final score and display
+    var finalScore = secondsRemain;
+
+    var createSpan = document.createElement("span");
+    createSpan.setAttribute("id", "createSpan");
+    createSpan.textContent = finalScore;
+
+    createP.appendChild(createSpan);
+
+    // User to enter initials in field
+    var createLabel = document.createElement("label");
+    createLabel.setAttribute("id", "createLabel");
+    createLabel.textContent = "Enter your initials: ";
+
+    questionsDiv.appendChild(createLabel);
+
+    var createInput = document.createElement("input");
+    createInput.setAttribute("type", "text");
+    createInput.setAttribute("id", "initials");
+    createInput.textContent = "";
+
+    questionsDiv.appendChild(createInput);
+
+    // Button to submit initials and score
+    var createSubmit = document.createElement("button");
+    createSubmit.setAttribute("type", "submit");
+    createSubmit.setAttribute("id", "submit");
+    createSubmit.textContent = "Submit";
+
+    questionsDiv.appendChild(createSubmit);
+
+    // Event listener for submitting initials and score
+    createSubmit.addEventListener("click", function () {
+      var initials = createInput.value;
+
+      // Ensure initials are not blank
+      if (initials === "") {
+        alert("Please enter your initials.");
+      } else {
+        // Get existing high scores from local storage or create empty array
+        var highScores = JSON.parse(localStorage.getItem("highScores")) || [];
+
+        // Add new high score object to the high scores array
+        var newScore = {
+          initials: initials,
+          score: finalScore,
+    };
+
+        highScores.push(newScore);
+
+        // Sort high scores in descending order
+        highScores.sort(function (a, b) {
+          return b.score - a.score;
+        });
+
+        // Save updated high scores to local storage
+        localStorage.setItem("highScores", JSON.stringify(highScores));
+
+        // Redirect to high scores page
+        window.location.href = "highscores.html";
+    )};
+    }
